959b02cf56db943bdfefd26f912a51af249a6565
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-13 13:40:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const makeChange = (c) => {
   // your name here
+	let obj ={}
+	let q=0, p=0,d=0,n=0;
+	obj['q'] = parseInt(c/25);
+	c = c%25;
+	obj['d'] =parseInt(c/10);
+	c=c%10;
+	obj['n']= parseInt(c/5);
+	c=c%5;
+	obj['p']= c;
+	return obj
 };
 
 // Do not the change the code below
